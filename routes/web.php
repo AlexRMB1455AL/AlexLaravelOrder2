c44420037b772c61814db9f422b8342e74a7f205
ALEKSANDR.I <alenamobilebkru@gmail.com>
2025-07-26 20:03:54 +0400

    migration

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+Route::get('/order', function () {
+    return view('order');
+});
+Route::get('/statistic', function () {
+    return view('statistic');
 });
